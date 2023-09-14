f0f7637613b571d72a84af17f0ec70f13d63b194
Ajaymalolan K V <115446924+Ajaymalolan@users.noreply.github.com>
2023-09-14 14:28:13 +0530

    Update RedShift.m

--- RedShift.m
+++ RedShift.m
@@ -1,17 +1,52 @@
+% importing starData.mat file
+
 load starData
+
+% finding number of rows/ number of observations
+
 nObs = size(spectra,1);
+
+% assignining initial wavelength and step size.
+
 lambdaStart = 630.02;
 lambdaDelta = 0.14;
+
+% calculating end wavelength
+
 lambdaEnd = lambdaStart + (nObs-1)*lambdaDelta;
+
+% storing array of wavelengths
+
 lambda = (lambdaStart:lambdaDelta:lambdaEnd)';
-s = spectra(:,2);
+
+% storing every value in spectra, you can load values of individual rows and columns
+
+s = spectra;
+
+% log scale plot and labels
+
 loglog(lambda,s,".-")
 xlabel("Wavelength")
 ylabel("Intensity")
+
+% finding minimum values
+
 [sHa,idx] = min(s);
+
+% storing minima value
+
 lambdaHa = lambda(idx);
+
+% marking minima values
+
 hold on
 loglog(lambdaHa,sHa,"rs","MarkerSize",8)
 hold off
+
+% finding redshift factor
+
 z = lambdaHa/656.28 - 1;
+
+% finding speed
+
 speed = z*299792.458;
