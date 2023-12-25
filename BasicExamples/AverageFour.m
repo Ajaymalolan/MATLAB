e8fb6a8e9a1c4a14544f3003301f828bece0916a
Ajaymalolan K V <115446924+Ajaymalolan@users.noreply.github.com>
2023-12-25 15:05:10 +0530

    Update AverageFour.m

--- BasicExamples/AverageFour.m
+++ BasicExamples/AverageFour.m
@@ -1,1 +1,18 @@
-
+num=input("number of data ");
+sum=0;
+average=0;
+for i=1:1:num
+    data = input("enter data ");
+    sum=sum+data;
+    if mod(i, 4) == 0
+        ave = sum/4;
+        sum=0;
+        disp("Average is "+ave);
+    else
+        if i == num && mod(num, 4) ~= 0
+        div=mod(i, 4);
+        ave=sum/div;
+        disp("Average is "+ave);
+        end
+    end
+end
